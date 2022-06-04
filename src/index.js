a616b3993fe6b08771404712e15d17a517f8f359
aragao <cristianoliveira2310@gmail.com>
2022-06-04 00:23:09 -0400

    feat: add playground

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 const { ApolloServer } = require("apollo-server");
 
+const {
+  ApolloServerPluginLandingPageGraphQLPlayground,
+} = require("apollo-server-core");
+
 const typeDefs = require("./typeDefs");
 const resolvers = require("./resolvers");
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  csrfPrevention: true,
+  plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+});
 
 server
   .listen({
